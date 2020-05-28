bd7fa373445ead8073f5cb6ea398a4403deee3d0
Anel CC <anel.calvo.cervantes@gmail.com>
2020-05-28 16:58:31 -0400

    [Anel][Dagger03] Create Application Component for Dagger where dagger knows where to inject dependency to.

---
+++ app/src/main/java/com/anelcc/mvp/AppComponent.java
@@ -0,0 +1,16 @@
+package com.anelcc.mvp;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+/*
+ * We need to create an application component where dagger knows where to inject dependency to.
+ * In Dagger2 injected class called components. These components assign references in our activity,
+ * services, and fragments to have access to the singleton which we are already defined.
+ * */
+
+@Singleton
+@Component(modules = {AppModule.class})
+public interface AppComponent {
+    void inject(MainActivity target);
+}
