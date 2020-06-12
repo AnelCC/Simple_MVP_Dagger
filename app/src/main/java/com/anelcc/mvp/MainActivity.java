b909e264ee9d906b3192a450b8338318aae88405
Anel CC <anel.calvo.cervantes@gmail.com>
2020-06-12 19:41:11 -0400

    [Anel][Dagger07] MVP Implementation start flushing out the methods provided by the view.

--- app/src/main/java/com/anelcc/mvp/MainActivity.java
+++ app/src/main/java/com/anelcc/mvp/MainActivity.java
@@ -3,15 +3,28 @@ package com.anelcc.mvp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.anelcc.mvp.app.App;
+import com.anelcc.mvp.login.LoginActivityMVP;
+
+import javax.inject.Inject;
 /*
 we need to define this injection, so that dagger components interface we will be used.
 Notice we are cast getApplication() to our App class. So now we get dagger component
 and inject activity.
 */
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoginActivityMVP.View {
+    @Inject
+    LoginActivityMVP.Presenter presenter;
+
+    private EditText firstName;
+    private EditText lastName;
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +32,52 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ((App) getApplication()).getComponent().inject(this);
+
+        firstName = (EditText) findViewById(R.id.loginActivity_firstName_editText);
+        lastName = (EditText) findViewById(R.id.loginActivity_lastName_editText);
+        login = (Button) findViewById(R.id.loginActivity_login_button);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        presenter.setView(this);
+    }
+
+    @Override
+    public String getFirstName() {
+        return firstName.getText().toString();
+    }
+
+    @Override
+    public String getLastName() {
+        return lastName.getText().toString();
+    }
+
+    @Override
+    public void showInputError() {
+        Toast.makeText(this, "First Name or last name cannot be empty", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void setFirstName(String firstName) {
+        this.firstName.setText(firstName);
+    }
+
+    @Override
+    public void setLastName(String lastName) {
+        this.lastName.setText(lastName);
+    }
+
+    @Override
+    public void showUserSavedMessage() {
+        Toast.makeText(this, "User saved successfully", Toast.LENGTH_SHORT).show();
     }
 }
