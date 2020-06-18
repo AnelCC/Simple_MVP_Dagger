a97b836fade5de1da1dea85c7b1e6d1820202b85
Anel CC <anel.calvo.cervantes@gmail.com>
2020-06-17 23:42:44 -0400

    [Anel][Dagger12] Add the another Module.class to the list of modules

--- app/src/main/java/com/anelcc/mvp/app/AppComponent.java
+++ app/src/main/java/com/anelcc/mvp/app/AppComponent.java
@@ -1,6 +1,7 @@
 package com.anelcc.mvp.app;
 
 import com.anelcc.mvp.MainActivity;
+import com.anelcc.mvp.login.LoginModule;
 
 import javax.inject.Singleton;
 
@@ -9,10 +10,13 @@ import dagger.Component;
  * We need to create an application component where dagger knows where to inject dependency to.
  * In Dagger2 injected class called components. These components assign references in our activity,
  * services, and fragments to have access to the singleton which we are already defined.
- * */
+ *
+ * because we are only accessing Module.class via our @Component annotation we also need to
+ * add the another Module.class to the list of modules. Eg. {Module1.class, Module2.class}
+ */
 
 @Singleton
-@Component(modules = {AppModule.class})
+@Component(modules = {AppModule.class, LoginModule.class})
 public interface AppComponent {
     void inject(MainActivity target);
 }
